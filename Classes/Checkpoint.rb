e95e7d45d21f030c18309889f7c64aeeeaded60c
Sollres <48085114+Sollres@users.noreply.github.com>
2020-02-07 11:37:57 +0100

    Add files via upload

---
+++ Classes/Checkpoint.rb
@@ -0,0 +1,56 @@
+class Checkpoint
+
+    private_class_method :new
+
+    def Checkpoint.creer(grille)
+        new(grille)
+    end
+
+    def initialize(grille)
+        #indide = 
+        @grille = grille
+        @check = Array.new()
+    end
+
+    def valider()
+        #retire les actions de la liste et les mets dans la liste d'action
+        #de la grille (@check.shift pour prendre le premier element)
+    end
+
+    def supprimer_derniere_action()
+        @check.pop
+    end
+
+    def supprimer_checkpoint()
+        i = 0
+        #Retire tout les elements sauf le dernier
+        while i <  @check.size #+ 2
+            @check.pop
+            i += 1
+        end
+        #Retire le dernier element
+        @check.pop
+    end
+
+    def emettre(valeur)
+        @check.push(valeur)
+    end
+
+    def afficher_stack()
+        puts "#{@check}"
+    end
+
+end
+
+checkpoint = Checkpoint.creer()
+
+checkpoint.emettre(5)
+checkpoint.emettre(4)
+checkpoint.supprimer_derniere_action
+checkpoint.emettre(10)
+checkpoint.emettre(6)
+checkpoint.afficher_stack()
+
+checkpoint.supprimer_checkpoint
+
+checkpoint.afficher_stack()
---
+++ Classes/Highscore.rb
@@ -0,0 +1,95 @@
+class Highscore
+    #@liste Liste de tout les highscores de la carte / Besoin de stocker le score avec le pseudo ou reference au pseudo
+    #@max Maximum de la liste afin de pouvoir placer de nouvelles valeurs
+    #@grille La grille sur laquelle le joueur joue
+    #@compte Reference au compte du joueur
+    private_class_method :new
+
+    def Highscore.creation(grille)
+        new(grille)
+    end
+
+    def initialize(grille)
+        @grille = grille
+        @liste = Hash.new()
+        @contenu = 0
+        @max = 0
+        @min = 0
+    end
+
+    def ajouter(cle,valeur)
+        if @contenu < 3
+            @liste[cle] = valeur
+            @contenu += 1
+        elsif @contenu >= 3
+            min = self.minimum()
+            @liste.delete(min)
+            @liste[cle] = valeur
+        else
+            puts "Impossible d'ajouter, nombre d'élements max atteint !\n\n"
+        end
+    end
+
+    def trie()
+        @liste.sort_by {|score, pseudo| score}
+        self.afficher
+    end
+
+    def maximum()
+        @liste.each_key do |key|
+            if(key>@max)
+                @max = key
+            end
+        end
+
+        return @max
+    end
+
+    def minimum()
+        @min = @liste.keys[0]
+        @liste.each_key do |key|
+            if(key<@min)
+                @min = key
+            end
+        end
+
+        return @min
+    end
+
+    def afficher()
+        puts "      Highscore       \n\n"
+        i = 0
+        taille = 1
+        @liste.each_pair do |key, value|
+            print key 
+            puts "=>" + value
+        end
+    end
+
+end
+
+
+tab = Highscore.creation
+=begin
+#Test ajout
+tab.ajouter(3500,"Soares")
+tab.ajouter(5000,"Soares")
+tab.ajouter(350,"Ordinateur")
+tab.afficher
+tab.ajouter(1550,"Ordi")
+#Test du max
+max = tab.maximum()
+puts max
+#Test du min
+min = tab.minimum()
+puts min
+#Test affichage
+tab.afficher
+=end
+
+tab.ajouter(1000, "Player 1")
+tab.ajouter(900, "Player 2")
+tab.ajouter(1100, "Player Test")
+tab.trie()
+tab.afficher
+
