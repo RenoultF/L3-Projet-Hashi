b880187a104bfc3d1434cb3eae895faff9be9821
JackGoodEnough <48084898+JackGoodEnough@users.noreply.github.com>
2020-02-07 12:10:17 +0100

    Add files via upload

--- Classes/VerifierGrille.rb
+++ Classes/VerifierGrille.rb
@@ -1,5 +1,6 @@
 load("Aide.rb")
 class VerifierGrille < Aide
+    attr:bTetard, false
     ##
     #marque les ponts à mettre en surbrillance
     #@param [grille] la grille du jeu
@@ -8,13 +9,33 @@ class VerifierGrille < Aide
     def demandeAide(grille, solution)
         res = true
         grille.mat.each do |caseG|
-            solution.mat.each do |caseS|
-                if(!caseG.equals(caseS) && caseG.valeur > 0)
-                    res = false
-                    caseG.surbrillance = true
-                end
-            end
+          caseS = solution.mat[caseG.posX][caseG.posY]
+          if(!caseG == (caseS) && caseG.valeur > 0)
+            res = false
+            caseG.surbrillance = true
+          end
+        end
+        if(res)
+          bTetard = true
+        else
+          bTetard = false
         end
         return res
+    end
+    ##
+    #verifie si l'ile est reliée correctement
+    #@param [Ile] Ile à verifier
+    #@param [Grille] La grille solution
+    #@return [boolean] vrai si bien reliée
+    def verifierIle(ile, solution)
+      return ile.grille.mat[ile.posX][ile.posY+1] == (solution.mat[ile.posX][ile.posY+1]) && ile.grille.mat[ile.posX][ile.posY-1] == (solution.mat[ile.posX][ile.posY-1]) && ile.grille.mat[ile.posX+1][ile.posY] == (solution.mat[ile.posX+1][ile.posY]) && ile.grille.mat[ile.posX-1][ile.posY] == (solution.mat[ile.posX-1][ile.posY])
+    end
+    ##
+    #verifie que l'ile est reliée aux bonnes iles, indépendamment du fait que ce soit des doubles pont ou pas.
+    #@param [Ile] Ile à verifier
+    #@param [Grille] La grille solution
+    #@return [boolean] vrai si bien reliée
+    def estAPeuPresBienReliee(ile, solution)
+      return ile.grille.mat[ile.posX][ile.posY+1] == (solution.mat[ile.posX][ile.posY+1]) && ile.grille.mat[ile.posX][ile.posY-1] == (solution.mat[ile.posX][ile.posY-1]) && ile.grille.mat[ile.posX+1][ile.posY] == (solution.mat[ile.posX+1][ile.posY]) && ile.grille.mat[ile.posX-1][ile.posY] == (solution.mat[ile.posX-1][ile.posY])
     end
 end
