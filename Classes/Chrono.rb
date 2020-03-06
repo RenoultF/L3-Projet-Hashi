1b0387a8108fe2492ebdec47d562877c79abce08
cjolliet <22954757+cjolliet@users.noreply.github.com>
2020-03-06 11:56:04 +0100

    Chrono.rb
    Je l'ai réécrit et tester !
    il est au point !
    ...
    Plus qu'à l'adapter au jeu...

---
+++ Classes/Chrono.rb
@@ -0,0 +1,58 @@
+##
+# Auteur LeNomDeLEtudiant
+# Version 0.1 : Date : Fri Mar 06 09:24:44 CET 2020
+#
+require 'gtk3'
+
+class Chrono
+
+	@minutes
+	@secondes
+	@active
+
+	def initialize()
+		@minutes = 0
+		@secondes = 0
+	end
+
+	def afficherTps()
+		if(@minutes < 10)
+			if(@secondes < 10)
+				return ("0#{@minutes}:0#{@secondes}")
+			else
+				return("0#{@minutes}:#{@secondes}")
+			end
+		else if(@secondes < 10)
+				return("#{@minutes}:0#{@secondes}")
+			else
+				return("#{@minutes}:#{@secondes}")
+			end
+		end
+	end
+
+	def activeChrono()
+		if (@active)
+			@active = false
+		else
+			@active = true
+		end
+	end
+
+	def lancerChrono()
+		self.activeChrono()
+		while( (@minutes < 59) && @active )
+			sleep(1)
+			@secondes += 1
+			if(@secondes >= 60)
+				@minutes += 1
+				@secondes = 0
+			end
+			puts self.afficherTps()
+		end
+	end
+
+end # Marqueur de fin de classe
+
+# Test Chrono
+# c = Chrono.new()
+# c.lancerChrono()
