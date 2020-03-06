6558fc126ec7d267f7e60f9f4de0d7c78ca8f77f
JackGoodEnough <48084898+JackGoodEnough@users.noreply.github.com>
2020-03-06 12:01:31 +0100

    wala non

--- Classes/Action.rb
+++ Classes/Action.rb
@@ -3,11 +3,12 @@ class Action
   attr:pont, false
   attr:grille, false
   attr:ajout, false
-  
+
   #:nodoc:
   def initialize(pont, grille, ajout)
     @pont = pont
     @grille = grille
+    @ajout = ajout
   end
 
   #:doc:
@@ -15,18 +16,21 @@ class Action
   ##
   #empile l'action dans la pile d'action de grille
   #@return : l'action empilée
-  def empiler()
-    grille.actions.push self
+  def empiler
+    @grille.actions.push self
   end
 
   ##
   #dépile l'action de la pile d'action de grille
   #@return : l'action dépilée
-  def depiler()
-    grille.actions.pop self
+  def depiler
+    @grille.actions.pop self
   end
 
+  ##
+  #retourne un tableau avec les variable d'action
+  #@return : tab[ajout(0:supr/1:ajout) , posX, posY, direction(1: horizontal / 2: vertical)]
   def getCoord
-    return pont.posX(), pont.posY()
+    return @ajout, @pont.posX(), @pont.posY(), @pont.direction()
   end
 end
