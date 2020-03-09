1e4b8cbe307d1788016ad94872ddb869b186e187
Brabant Mano <s171085@ic2s114-20.univ-lemans.fr>
2020-03-09 17:50:23 +0100

    ptit ajout

--- Classes/Ile.rb
+++ Classes/Ile.rb
@@ -1,6 +1,22 @@
 load "Case.rb"
 
 class Ile < Case
+
+
+
+    #Cette constante représente l'une des directions dans laquelle peut se trouver un voisin
+    HAUT = 0
+
+    #Cette constante représente l'une des directions dans laquelle peut se trouver un voisin
+    DROITE = 1
+
+    #Cette constante représente l'une des directions dans laquelle peut se trouver un voisin
+    BAS = 2
+
+    #Cette constante représente l'une des directions dans laquelle peut se trouver un voisin
+    GAUCHE = 3
+
+
     include Comparable
     private_class_method :new
 
@@ -12,7 +28,7 @@ class Ile < Case
         super(posX, posY, grille)
         @valeur = nbLien
         @nbPont = 0
-        
+
     end
 
     def getValeur()
@@ -43,5 +59,49 @@ class Ile < Case
         end
         return 0
     end
+
+
+    def getVoisin(direction)
+
+      indice = 1
+
+      if(direction == Ile::HAUT)
+        until(@grille.sortLimite(@posX, @posY - indice) or @grille.getGrille()[@posX][@posY - indice].instance_of? Ile)
+          indice += 1
+        end
+        posX = @posX
+        posY = @posY - indice
+      elsif(direction == Ile::BAS)
+        until(@grille.sortLimite(@posX, @posY + indice) or @grille.getGrille()[@posX][@posY + indice].instance_of? Ile)
+          indice += 1
+        end
+        posX = @posX
+        posY = @posY + indice
+      elsif(direction == Ile::GAUCHE)
+        until(@grille.sortLimite(@posX - indice, @posY) or @grille.getGrille()[@posX - indice][@posY].instance_of? Ile)
+          indice += 1
+        end
+        posX = @posX - indice
+        posY = @posY
+      elsif(direction == Ile::DROITE)
+        until(@grille.sortLimite(@posX + indice, @posY) or @grille.getGrille()[@posX + indice][@posY].instance_of? Ile)
+          indice += 1
+        end
+        posX = @posX + indice
+        posY = @posY
+      else
+        raise("Cette ile n'a pas de voisins")
+      end
+
+      return @grille.getGrille()[posX][posY]
+
+    end
+
+    def aVoisin(direction)
+
+      return getVoisin(direction).instance_of? Ile
+
+    end
+
 
 end
