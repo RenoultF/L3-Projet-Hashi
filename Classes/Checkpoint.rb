609b6f4ef69f51c9c1c8ec779e75466d81b3c71a
RenoultF <59865290+RenoultF@users.noreply.github.com>
2020-03-13 16:25:13 +0100

    Add files via upload

--- Classes/Checkpoint.rb
+++ Classes/Checkpoint.rb
@@ -11,23 +11,16 @@ class Checkpoint
         @check = Array.new()
     end
 
-    ##
-    #valide l'hypothese et l'ajoute dans la pile d'action
     def valider()
         @longueurpileAvant = nil
-        until checkpointVide()
-            @grille.actions.push(@check.shift)
-        end
+        #retire les actions de la liste et les mets dans la liste d'action
+        #de la grille (@check.shift pour prendre le premier element)
     end
 
-    ##
-    #Supprime a derniere action réalisée dans l'hypothese
     def supprimer_derniere_action()
         @check.pop
     end
 
-    ##
-    #Supprime l'hypothese entierement si pleine fait rien sinon
     def supprimer_checkpoint()
         #Retire tout les elements sauf le dernier
         if(@longueurpileAvant != nil)
@@ -39,20 +32,14 @@ class Checkpoint
         end
     end
 
-    ##
-    #Permettre d'emettre une hypothese dans le checkpoint
     def emettre()
         @longueurpileAvant = @grille.getAction().size()
     end
 
-    ##
-    #Permet d'afficher l'hypothese
     def afficher_stack()
         puts "#{@check}"
     end
 
-    ##
-    #Verifie si le checkpoint est vide
     def checkpointVide()
         return @check.empty?
     end
@@ -66,15 +53,15 @@ class Checkpoint
 
 end
 
-#checkpoint = Checkpoint.creer()
+checkpoint = Checkpoint.creer()
 
-#checkpoint.emettre(5)
-#checkpoint.emettre(4)
-#checkpoint.supprimer_derniere_action
-#checkpoint.emettre(10)
-#checkpoint.emettre(6)
-#checkpoint.afficher_stack()
+checkpoint.emettre(5)
+checkpoint.emettre(4)
+checkpoint.supprimer_derniere_action
+checkpoint.emettre(10)
+checkpoint.emettre(6)
+checkpoint.afficher_stack()
 
-#checkpoint.supprimer_checkpoint
+checkpoint.supprimer_checkpoint
 
-#checkpoint.afficher_stack()
+checkpoint.afficher_stack()
